e40728deaa2b455333ddd6372726fbb5b49664ca
oly <o.bausch@levigo.de>
2023-09-06 15:31:39 +0200

    Add files via upload

---
+++ SENEC/module.php
@@ -0,0 +1,86 @@
+<?php
+    // Klassendefinition
+    class SymconSenecData extends IPSModule {
+ 
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+            // Diese Zeile nicht löschen.
+            parent::Create();
+
+            $this->RegisterPropertyString("SENEC_API_Base_Url", "https://app-gateway-prod.senecops.com/v1/senec/");
+            $this->RegisterPropertyString("SENEC_API_Login_Stub", "login");
+            $this->RegisterPropertyString("SENEC_API_Anlagen_Stub", "anlagen");
+
+            $this->RegisterVariableString("SENEC_Token", "Access Token");
+	    }   
+		
+
+        // Überschreibt die intere IPS_ApplyChanges($id) Funktion
+        public function ApplyChanges() {
+            // Diese Zeile nicht löschen
+            parent::ApplyChanges();
+        }
+ 
+
+        /**
+        * Die folgenden Funktionen stehen automatisch zur Verfügung, wenn das Modul über die "Module Control" eingefügt wurden.
+        * Die Funktionen werden, mit dem selbst eingerichteten Prefix, in PHP und JSON-RPC wiefolgt zur Verfügung gestellt:
+        *
+        * VMC_TTS($monkey_device, $text)
+        */
+
+        // -------------------------------------------------------------------------        
+        public function GetToken() {
+
+            define('USER_AGENT', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (K HTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
+ 
+
+            $baseurl   = $this->ReadPropertyString("SENEC_API_Base_Url");
+            $loginstub = $this->ReadPropertyString("SENEC_API_Login_Stub");
+            $username  = $this->ReadPropertyString("SENEC_API_Username");            
+            $password  = $this->ReadPropertyString("SENEC_API_Password");            
+
+            $credentials = '{
+                "username": $username,
+                "password": $password
+            }';
+
+            $curl = curl_init();                                                            // los geht's
+
+            curl_setopt($curl, CURLOPT_URL, $baseurl."/".$loginstub);                                     // URL zum Loginformular
+            curl_setopt($curl, CURLOPT_POST, true);                                         // Ein POST request soll es werden
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $credentials);                           // Die Infos als JSON Body schicken
+            
+            curl_setopt($curl, CURLOPT_USERAGENT, USER_AGENT);                              // Hilft bei einer eventuellen Sessionvalidation auf Serverseite
+
+            curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 0);                                  // keine Prüfung ob Hostname im Zertifikat
+            curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);                              // keine Überprüfung des Peerzertifikats
+            curl_setopt($curl, CURLOPT_FOLLOWLOCATION, false);                              // redirects folgen
+
+
+            curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);                               // Die Antwort bitte nicht an STDOUT
+
+
+
+            $headers = [
+                'Content-Type: application/json'
+            ];
+
+            curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+
+
+            $response = curl_exec($curl);                                                               // ok, jetzt ausführen
+
+            $curl_errno = curl_errno($curl);
+
+            if ($curl_errno > 0) {
+                $curl_error = curl_error($curl);
+            } else {
+                $token = json_decode($response, true)['token'];
+    			$this->SetValue("SENEC_Token", $token);
+            }
+            curl_close($curl);                                                              // cURL Session beenden
+      	}
+
+    }
+?>
