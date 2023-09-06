b373b1931f3ccb38af15394976fe383067b5ca63
oly <o.bausch@levigo.de>
2023-09-06 20:43:15 +0200

    copy & paste katastrophe

--- SENEC/module.php
+++ SENEC/module.php
@@ -7,8 +7,8 @@
             // Diese Zeile nicht löschen.
             parent::Create();
 		
-            $this->RegisterPropertyString("SENEC_API_Username", "" };
-	    $this->RegisterPropertyString("SENEC_API_Password", "" };
+            $this->RegisterPropertyString("SENEC_API_Username", "");
+	    $this->RegisterPropertyString("SENEC_API_Password", "");
 		
             $this->RegisterPropertyString("SENEC_API_Base_Url", "https://app-gateway-prod.senecops.com/v1/senec");
             $this->RegisterPropertyString("SENEC_API_Login_Stub", "login");
